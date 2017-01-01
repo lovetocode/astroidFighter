738fec1e3126b92128c0a7d78f99702ce237764e
Daniel <cranky30@hotmail.com>
2017-01-01 20:26:20 +0800

    first commit

---
+++ astroidFighter.py
@@ -0,0 +1,10 @@
+# made with python 3
+
+# None -> Strings
+# introduces the player to the game
+# def intro(): 
+#tests
+# template
+def intro():
+	print("Hello and welcome to one of the more unpleasant gameing")
+	print("experiences you will ever have."
