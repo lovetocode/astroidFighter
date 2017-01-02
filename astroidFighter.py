9af5ab2a544c8bea9eab7e0223fa1c25f815caa7
Daniel <cranky30@hotmail.com>
2017-01-02 20:31:14 +0800

    updates

--- astroidFighter.py
+++ astroidFighter.py
@@ -1,5 +1,9 @@
 # made with python 3
 
+skillLevel = "p"
+missle = 0
+play = True
+
 # None -> Strings
 # introduces the player to the game
 # def intro(): 
@@ -7,4 +11,21 @@
 # template
 def intro():
 	print("Hello and welcome to one of the more unpleasant gameing")
-	print("experiences you will ever have."
+	print("experiences you will ever have, prepare to be underwelmed")
+	print("skill level. Sorry there is only novice for new.")
+	skillLevel = input()
+	skill(skillLevel)
+
+# string -> string
+# determines the skill level
+# def skill(skillLevel):
+#Test
+# Template
+def skill(s):
+	if s == "novice" :
+		missle = 5
+		print("hello")
+	else: print("Why are you here")
+intro()
+
+
